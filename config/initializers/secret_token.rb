13cb7b55a05042a3298b0c0af276a6766386bb1e
asonas <hzw1258@gmail.com>
2013-02-17 23:20:34 +0900

    Step1: アプリケーションを作る

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = 'f99893cffb2f43b012c977e42242e0bd12baf8ade0c9fd66bdc71f0d3223d4fa840da0023b1b18b1f59e53d2e516842f9930de4aa520fca9b292df137063571c'
